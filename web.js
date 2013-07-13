557cbe3152e5eaf1456682059612d81ae987cfa3
RedR <redr@gmail.com>
2013-07-13 18:16:55 +0000

    Fixed web.js - buf.toString() is called from buf context instead of general context

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  reponse.send(buf.toString(fs.readFileSync(index.html)));
+  reponse.send(fs.readFileSync(index.html).toString());
 });
 
 var port = process.env.PORT || 5000;
