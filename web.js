2d8756aba803798dbf6a48a1aadf85e2d15541dd
RedR <redr@gmail.com>
2013-07-13 19:15:08 +0000

    Found the bug. It was a typo - 'reponse' instead of 'response'. Sheesh.

--- web.js
+++ web.js
@@ -4,8 +4,7 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  indexout = fs.readFileSync('index.html').toString();
-  reponse.send(indexout);
+  response.send(fs.readFileSync('index.html').toString());
 });
 
 var port = process.env.PORT || 5000;
